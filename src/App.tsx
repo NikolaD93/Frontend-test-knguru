7556f06e9794ab24eab1408eb92f6b378d15685a
Nikola Dojcinovic <goonergalactico@gmail.com>
2025-05-13 21:02:15 +0200

    FEAT: project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1 className="my-2 mt-2 flex justify-around rounded px-4 py-2 pb-0 text-3xl">Hello</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
